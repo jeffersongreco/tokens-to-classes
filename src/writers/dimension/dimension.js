5645aac59b4b353fbc499d2a8202e7f3c9961689
Greco <greco.grco@gmail.com>
2024-02-01 16:44:15 -0400

    first commit

--- src/writers/dimension/dimension.js
+++ src/writers/dimension/dimension.js
@@ -3,24 +3,23 @@ import camelToKebab from '../../utils/camel-to-kebab.js'
 function dimensionClasses(tokensJson, classesJson) {
   let classes = [];
 
-  function processObject(tokens, classes, path = "") {
-    for (let classKey in classes) {
-      let prefix = classKey;
-      let properties = classes[classKey];
+  function processObject(tokensJson, classesJson, path = "") {
+    for (let classKey in classesJson) {
+      let prefix = `.${classKey}`;
+      let properties = classesJson[classKey];
 
-      for (let tokenKey in tokens) {
-        let className = `.${prefix}${path}-${camelToKebab(tokenKey)}`;
+      for (let tokenKey in tokensJson) {
+        let className = `${prefix}${path}-${camelToKebab(tokenKey)}`;
         let variableName = `--uai-dimension${path}-${camelToKebab(tokenKey)}`;
-        let value = tokens[tokenKey];
+        let value = tokensJson[tokenKey];
 
         if (typeof value === "object") {
           processObject(value, properties, `${path}-${camelToKebab(tokenKey)}`);
         } else {
           classes.push(`${className} {`);
-          console.log(className)
 
-          for (let property of properties) {
-            classes.push(`${property}: var(${variableName});`);
+          for (let property in properties) {
+            classes.push(`  ${property}: var(${variableName});`);
           }
 
           classes.push(`}`);
@@ -38,10 +37,13 @@ export default dimensionClasses;
 
 import fs from 'fs';
 import getJson from '../../utils/get-json.js';
+
 const tokensPath = '../../../tokens/dimension.tokens.json';
 const tokensJson = getJson(tokensPath);
 const classesPath = './classes.json';
 const classesJson = getJson(classesPath);
+
 const result = dimensionClasses(tokensJson, classesJson);
+
 console.log(result);
 // fs.writeFileSync('./tmp.css', result, 'utf-8');
